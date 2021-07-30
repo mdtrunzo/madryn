b80231c41b558c5151d555efb9d1a5fd2f81cbfa
Emiliano Carasa <emilianocarasa@gmail.com>
2021-07-30 17:07:22 -0300

    demo

--- backend_v1/app/Http/Controllers/CuponesController.php
+++ backend_v1/app/Http/Controllers/CuponesController.php
@@ -16,14 +16,20 @@ class CuponesController extends Controller
     public function getCupon(Request $request)
     {
 
-         return json_encode(array(
+         $cupon = array(
             "id" => 1,
             "prestador"=> "Nievemar",
             "premio"=> "2x1 en excursión regular (medio día) a El Doradillo o en su defecto (por fin de temporada) a Punta Loma",
             "direccion"=> "Italia 20, Trelew, Chubut",
             "telefono"=> "0280 443-4114",
             "img"=> "img/nievemar.png"
-         ));
+         );
+
+         return response()->json($cupon);
+
+
+
+         
 
         // validamos que vengan los datos del form
         $validator = Validator::make($request->all(), [
