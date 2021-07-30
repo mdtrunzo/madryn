cd2fe51736ea51e1938e977f7f4e051706556fc5
Matias <matiasdanieltrunzo@gmail.com>
2021-07-29 21:04:46 -0300

    Fetch post

--- js/index.js
+++ js/index.js
@@ -5,22 +5,39 @@ const lastName = document.getElementById('lastName')
 const email = document.getElementById('email')
 const submit = document.getElementById('submit')
 
-submit.addEventListener('click', (e) => {
+// Fetch to API
+const url = 'https://hookb.in/aBBkK8RgW6tXQ9kkQoxa'
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault()
+  //Save to LS
   const nameValue = name.value
   const lastNameValue = lastName.value
   const emailValue = email.value
 
-  //Save in local storage
   const nameStorage = localStorage.setItem('Name', nameValue)
   const lastNameStorage = localStorage.setItem('Last Name', lastNameValue)
   const emailStorage = localStorage.setItem('Email', emailValue)
 
-  name.value = ''
-  lastName.value = ''
-  email.value = ''
+  //Fetch
+  const formData = new FormData(form)
+  const formDataSerial = Object.fromEntries(formData)
+  const jsonObject = {
+    ...formDataSerial
+  }
 
-  window.location.href = "voucher.html";
-
-  e.preventDefault()
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(jsonObject),
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+    })
+    const json = await response.json()
+    window.location.href = "voucher.html";
+  } catch (err) {
+    console.log(err)
+  }
 })
-
