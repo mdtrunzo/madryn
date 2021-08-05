feb585ad16b49e0e335e4f7c451c7e5fbe3d0f7c
Matias <matiasdanieltrunzo@gmail.com>
2021-08-05 13:17:34 -0300

    voucher

--- js/pdf.js
+++ js/pdf.js
@@ -7,28 +7,27 @@ button.addEventListener('click', makePDF)
 
 
 function makePDF() {
-  if (screen.width < 1024) {
-    document.getElementById("viewport").setAttribute("content", "width=800px");
-  }
-  var scaleBy = 1;
-  var w = 1000;
-  var h = 1000;
-  var div = document.querySelector('#screen');
-  var canvas = document.createElement('canvas');
-  canvas.width = w * scaleBy;
-  canvas.height = h * scaleBy;
-  canvas.style.width = w + 'px';
-  canvas.style.height = h + 'px';
-  var context = canvas.getContext('2d');
-  context.scale(scaleBy, scaleBy);
+  // var scaleBy = 1;
+  // var w = 1000;
+  // var h = 1000;
+  // var div = document.querySelector('#screen');
+  // var canvas = document.createElement('canvas');
+  // canvas.width = w * scaleBy;
+  // canvas.height = h * scaleBy;
+  // canvas.style.width = w + 'px';
+  // canvas.style.height = h + 'px';
+  // var context = canvas.getContext('2d');
+  // context.scale(scaleBy, scaleBy);
   html2canvas(document.querySelector("#capture"), {
     allowTaint: true,
     useCORS: true,
-    scale: 4
+    scale: 2,
+    scrollY: -window.scrollY
   }).then(canvas => {
     var img = canvas.toDataURL('image/png')
     var doc = new jsPDF()
-    doc.addImage(img, 'PNG', 20, 20)
+    doc.addImage(img, 'PNG', 45, 13, 120, 165)
     doc.save('Voucher')
+    doc.output('dataurlnewwindow');
   });
 }
--- js/voucher.js
+++ js/voucher.js
@@ -5,39 +5,6 @@ const containerVoucher = document.querySelector('.container-voucher')
 window.onload = function getContent() {
   loadingPage()
   output.innerHTML = localStorage.getItem('html_form')
-  // fetch('js/db.json')
-  //   .then(res => {
-  //     return res.json()
-  //   })
-  //   .then(data => {
-  //     data.forEach(post => {
-  //       output.innerHTML += `
-  //       <div class="usuario-ganador">
-  //         <h2>${(localStorage.getItem('Name').toUpperCase())} ${(localStorage.getItem('Last Name').toUpperCase())}</h2>
-  //         <p>${localStorage.getItem('Email')}</p>
-  //       </div>
-  //       <div class="premio">
-  //         <div class="ganaste-container">
-  //          <span class="ganaste">GANASTE</span>
-  //         </div>
-  //         <p class="premio-title">${post.premio}<br>en</br></p>
-  //         <div class="local-voucher">
-  //            <div class="imagen-voucher">
-  //              <img src="${post.img}" width="200">
-  //             </div>
-  //            <div class="info-voucher">
-  //              <h2>${post.prestador}</h2>
-  //              <p>Tel: ${post.telefono} </p>
-  //              <p>Dirección: ${post.direccion}</p>
-  //            </div>
-  //         </div>
-  //       </div>
-  //   `
-  //     })
-  //   })
-  //   .catch(function (err) {
-  //     console.log(err)
-  //   })
 }
 
 function loadingPage() {
